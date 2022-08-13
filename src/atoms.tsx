0309409a688db100e4c9bac5e1a07ced9858799d
chromeheartz <barnesquiat@gmail.com>
2022-08-13 21:04:08 +0900

    test m

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,22 +1,7 @@
 import { atom, selector } from "recoil";
 
-export const minuteState = atom({
-  key : "minutes",
-  default : 0,
+export const toDoState = atom({
+  key : "toDo",
+  default : ["a", "b", "c", "d", "e", "f"],
 })
 
-// 이 selector는 number만을 return
-export const hourSelector = selector<number>({
-  key : "hours",
-  // selector안에서 atom에 접근하려면 get함수를 사용
-  get : ({ get }) => {
-    const minutes = get(minuteState);
-    return minutes / 60;
-  },
-  // atom을 수정하는것을 도와주는 set
-  set : ({ set }, newValue) => {
-    const minutes = Number(newValue) * 60;
-    // set 함수는 2개의 인자를 받음. 수정하고싶은 recoil atom, 새로운값
-    set(minuteState, minutes)
-  }
-})
