d9616481024cdaeccd896e614b899aaea23670b9
chromeheartz <barnesquiat@gmail.com>
2022-08-14 16:25:30 +0900

    #6.8 multi boards

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from "react";
-import { DragDropContext, Droppable, DropResult } from "react-beautiful-dnd";
+import { DragDropContext, DropResult } from "react-beautiful-dnd";
 import { useRecoilState } from "recoil";
 import styled from "styled-components";
 import { toDostate } from "./atoms";
-import DragabbleCard from "./Componenets/DragabbleCard";
+import Board from "./Componenets/Board";
 
 const Wrapper = styled.div`
   display : flex;
-  max-width : 480px;
+  max-width : 680px;
   width : 100%;
   margin : 0 auto;
   justify-content : center;
@@ -18,20 +18,9 @@ const Wrapper = styled.div`
 const Boards = styled.div`
   display : grid;
   width : 100%;
-  grid-template-columns : repeat(1, 1fr);
+  grid-template-columns : repeat(3, 1fr);
+  gap : 10px;
 `
-
-const Board = styled.div`
-  padding : 20px 10px;
-  padding-top : 30px;
-  background-color : ${props => props.theme.boardColor};
-  border-radius :5px;
-  min-height : 200px;
-`
-
-
-
-const toDos = ["a", "b", "c", "d", "e", "f"]
 
 function App() {
   // return되는 배열의 첫번째는 atom의 값, 두번쨰는 atom을 수정하는함수
@@ -41,66 +30,23 @@ function App() {
     // destination, source 로 어디에서 어디로가는지도 알수있음.
     // dettnation이 없을경우 (같은 자리에 둘경우)
     if(!destination) return
-    /*
-      typescript의 에러수정방법
-      onDrageEnd를 보면 어떻게 생겼는지 알려주는데
-      result와 provided라는 인수를 return한다.
-
-      그 안의 DropList를 열어보면 destination과 같은 것들을
-      다 가져와야 하는것을 볼 수 있을것이다.
-
-      그래서 type을 DropResult로 정해주고 import를 한다
-
-      splice의 두개의 인수는 지우는것을 시작할 위치, 지울아이템의갯수
-      세개의 인수는 앞의 두개와 같은데 두번째에 0 을넣고 세번째에 아이템을 넣어주면
-      그 자리에 들어간다
-
-      * splice는 그 array 자체를 수정한다
-
-      * setToDos는 atom의 값을 수정할 2가지 방법을 전달해준다.
-      하나는 그냥 값을 보내주는것이고
-      다른 하나는 현재의 값을 arg로 주고 새로운 state를 return하는 함수를 주는것
-
-      ** 우리가 map을 할때 key 에 Number를 주는것이 익숙하지만
-      key와 draggableId는 무조건 같아야한다
-    */
-    /*
-      // 1) Delete item on source.index
-      console.log("Delete item on", source.index);
-      console.log(toDosCopy);
-      toDosCopy.splice(source.index, 1);
-      console.log("Deleted item");
-      console.log(toDosCopy);
-      // 2) Put back the item on the destination.index
-      console.log("Put back", draggableId, "on ", destination.index);
-      toDosCopy.splice(destination?.index, 0, draggableId);
-      console.log(toDosCopy);
-    */
-    setToDos(oldToDos => {
-      // mutate할 수 없기 때문에 카피해서 unpacking
-      const toDosCopy = [...oldToDos];
-      // delete item on source.index 
-      toDosCopy.splice(source.index, 1)
-      // put back the item on the destination.index
-      // draggableId 는 toDo
-      toDosCopy.splice(destination?.index, 0, draggableId);
-      return toDosCopy;
-    })
+   
   };
   return (
     <DragDropContext onDragEnd={onDragEnd}>
       <Wrapper>
         <Boards>
-          <Droppable droppableId="one">
-            {(magic) => (
-              <Board ref={magic.innerRef} {...magic.droppableProps}>
-                {toDos.map((toDo,index) => (
-                  <DragabbleCard key={toDo} index={index} toDo={toDo} />
-                ))}
-                {magic.placeholder}
-              </Board>
-            )}
-          </Droppable>
+          {/*
+            IBoardProps.toDos : string[]
+            이 arror는 toDos는 string으로 이루어진 array인데
+            toDos boardId는 typescript에 의하면 string이 아니여서이다.
+            typescript는 현재 to_do, doing, done 이 세가지 선택지밖에 없기 때문
+            그래서 typescript에게 toDoState가 무엇인지 알려줄것이다
+            나중에 유저가 추가할수도 있기 때문
+          */}
+          {Object.keys(toDos).map((boardId) => (
+            <Board boardId={boardId} key={boardId} toDos={toDos[boardId]} />
+          ))}
         </Boards>
       </Wrapper>
     </DragDropContext>
@@ -150,6 +96,67 @@ export default App;
   사이즈가 이상하게 변하지 않을것이다.
   이상하게 사이즈가 변하는 Board의 끝에서
   {magic.placeholder}를 둘것이다
+
+*/
+
+
+/*
+  typescript의 에러수정방법
+  onDrageEnd를 보면 어떻게 생겼는지 알려주는데
+  result와 provided라는 인수를 return한다.
+
+  그 안의 DropList를 열어보면 destination과 같은 것들을
+  다 가져와야 하는것을 볼 수 있을것이다.
+
+  그래서 type을 DropResult로 정해주고 import를 한다
+
+  splice의 두개의 인수는 지우는것을 시작할 위치, 지울아이템의갯수
+  세개의 인수는 앞의 두개와 같은데 두번째에 0 을넣고 세번째에 아이템을 넣어주면
+  그 자리에 들어간다
+
+  * splice는 그 array 자체를 수정한다
+
+  * setToDos는 atom의 값을 수정할 2가지 방법을 전달해준다.
+  하나는 그냥 값을 보내주는것이고
+  다른 하나는 현재의 값을 arg로 주고 새로운 state를 return하는 함수를 주는것
+
+  ** 우리가 map을 할때 key 에 Number를 주는것이 익숙하지만
+  key와 draggableId는 무조건 같아야한다
+*/
+/*
+  // 1) Delete item on source.index
+  console.log("Delete item on", source.index);
+  console.log(toDosCopy);
+  toDosCopy.splice(source.index, 1);
+  console.log("Deleted item");
+  console.log(toDosCopy);
+  // 2) Put back the item on the destination.index
+  console.log("Put back", draggableId, "on ", destination.index);
+  toDosCopy.splice(destination?.index, 0, draggableId);
+  console.log(toDosCopy);
+*/
+
+/*
+  object 를 loop하는 방법
+  const toDos = {
+    x : ["a", "b"],
+    z : ["n", "t"]
+  }
+  이런식의 object가 있는경우
+  각 property들을 받아서 그 array를 render하고싶다
+
+  그런때에 Object.keys를 해주면 가진 key만 array로 뽑아준다
+  Object.keys(toDos) = ['x', 'z']
+
+  마찬가지로 Object.values(toDos) 하면 value들만 가져온다
+
+  * toDos['x']라고 하면 x의 value값들이 나올것이다
+  ["a", "b"]
+
+  Object.keys(toDos).map(boardId => toDos[boardId])
+  이런식으로 응용해서 사용해주면
+  boardId가 있고 그 boardId가 각각 가지는 자식들도 볼 수 있게 되었다.
+
 
 
 
--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,7 +1,22 @@
 import { atom, selector } from "recoil";
 
-export const toDostate = atom({
+interface IToDoState {
+  /*
+
+    이 state는 
+    우리의 key (to_do, doing, done)은 단지 key이고, 그것은 string니자<div className=""></div>
+    (string으로써의 property)
+    그리고 string array 로 이루어져있다 고 말한것
+  */
+  [key : string] : string[]
+}
+
+export const toDostate = atom<IToDoState>({
   key : "toDo",
-  default : ["a", "b", "c", "d", "e", "f"],
+  default : {
+    to_do : ["a", "b", "c", "d", "e", "f"],
+    doing : [],
+    done : [],
+  },
 })
 
