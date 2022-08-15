18732242d2d02de2eaaafa263d999d81e072143d
chromeheartz <barnesquiat@gmail.com>
2022-08-15 14:08:08 +0900

    #7.2 basic animation

--- src/recap/recap.jsx
+++ src/recap/recap.jsx
@@ -23,4 +23,33 @@
   craco-config.js에 설정을 넣어주고
   package.json 에서 start,build,test를
   'react-scripts' 라는 단어를 craco로 바꾸어주어야한다.
+
+  #7.2 basic animation
+  styled component랑 motion을 합치려면
+  styled(motion."HTMLTag") 이런식으로 써주면 된다
+
+  그 component에 prop으로 속성을던질 수 있음.
+  예 )
+    animate={{borderRadius : "100px"}}
+    transition={{duration : 3, delay : 1}}
+
+  initial은 내가 원하는상태, element의 초기상태
+
+  * 애니메이션을 걸었는데 살짝 바운스(튕기는)느낌이 있는것은
+  모든 애니메이션에는 spring이라는게 기본적으로 달려있기 때문
+
+  transition의 타입을 tween으로 해주면 선형적으로 진행된다
+  spring으로 하면 살짝 튕김
+
+  spring타입중에는 stiffness가 있는데 이것은 물리현상을 약간 시뮬레이트한다
+  stiff(뻣뻣)하게 만듬
+  damping(반동력) 같은것도 사용가능. 0으로 설정하면 무기한으로 진동
+  (운동에 저항이 없는것)
+
+  * spring은 현실세계의 물리법칙을 시뮬레이트한다
+  force(힘)이 있고 elasticity(탄력성), stiffness(경직됨)이 있는것
+  
+  문서를 보면 transition의 속성들도 볼수 있다
+
+
 */
