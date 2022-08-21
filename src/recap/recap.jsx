31b75e0f9dc20741a60f8661903c82ddb07eeb29
chromeheartz <barnesquiat@gmail.com>
2022-08-21 10:40:31 +0900

    #8.2 Header part Two

--- src/recap/recap.jsx
+++ src/recap/recap.jsx
@@ -1,5 +1,57 @@
 /*
   
+  #8.1 ~ #8.4 Header
+
+  * 계속 반복되는 애니메이션  
+  logo부분에 initial값과 animate값으로 마우스가 오버되었을때
+  채워주는색상을 0으로 만들었다. 하지만 계속해서 반복시켜주고싶다면
+  하나의 값을 가지는 대신에 배열로 만들 수 있다.
+
+  active : {
+    fillOpacity : [0, 0.5, 0, 0.7, 0, 0.2, 1, 0],
+    transition : {
+      repeat : Infinity,
+    }
+  },
+
+  이렇게 각 단계를 지정하면, motion이 모든 단계를 실행시켜줄것이다.
+  또한 transition repeat을 통해 무한으로할건지 아니면 반복횟수를 5 수로도 넣어줄수있다.
+
+  ** router
+
+  간혹 Route path 가 / 인 메인페이지인 것을 제일 상단에 올려놓으면
+  다른 서브로들어가게 될때 "/"가 먼저 매칭되면서 주소는 옮겨지지만
+  안에 컴포넌트가 제대로 출력이 되지 않을때가 있다. 그럴때에는
+  /만 있는 경로를 제일 하단에 넣어주면 된다.
+
+  ** route match
+
+  react router는 우리가 지금 찾고있는 route는 URL에 있을것이라고 말해주고있다.
+
+  useRouteMatch는 우리에게 이 route안에 있는지 다른곳에 있는지를
+  알려줄것이다.
+
+  const homeMatch = useRouteMatch("/")
+  const tvMatch = useRouteMatch("/tv")
+  console.log(homeMatch, tvMatch)
+
+  여기서도 결과값이 
+  {path: '/', url: '/', isExact: false, params: {…}}
+  {path: '/tv', url: '/tv', isExact: true, params: {…}}
+
+  {path: '/', url: '/', isExact: true, params: {…}}
+  null
+
+  path: '/', 항상 이 경로를 가지고있다. 이것은 항상 true가 될것이다
+  / 안에서 /tv안에 있는것이기때문
+  현재 보면 home을 클릭해서 가게되면 homeMatch의 isExact는 true가 되고
+  tvMatch가 null 이 될것이다. 
+
+  여기서 실질적으로 하고싶은것은 단순히 tvMatch가 존재하는지 여부에 따라 원을 그린다.
+  하지만 homeMatch는 항상 가지고있기 때문에 그 안의 isExact를 확인해야한다.
+
+  Home {homeMatch?.isExact && <Circle />}
+  Tv Shows{tvMatch && <Circle />}
 
 */
 
